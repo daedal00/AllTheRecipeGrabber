847b0510ffcd280198bb5811ca54ba6564993a21
daedal00 <92826197+daedal00@users.noreply.github.com>
2023-09-16 15:41:33 -0700

    improved functionality and styling

--- script.js
+++ script.js
@@ -3,22 +3,29 @@ document.addEventListener("DOMContentLoaded", function() {
     const ingredientInput = document.getElementById('ingredientInput');
     const resultsList = document.getElementById('resultsList');
     const detailsContainer = document.getElementById('detailsContainer');
+    const closeDetails = document.getElementById('closeDetails');
 
     searchButton.addEventListener('click', function() {
+        detailsContainer.style.display = 'none';
+        detailsContainer.innerHTML = '';
+        resultsList.style.display = 'none';
         const ingredients = ingredientInput.value;
         searchRecipes(ingredients).then(displayResults);
     });
 
     async function searchRecipes(ingredients) {
         const response = await fetch(`https://alltherecipeapi.onrender.com/search?ingredients=${ingredients}`);
+        // const response = await fetch(`http://localhost:5000/search?ingredients=${ingredients}`);
         const data = await response.json();
 
         return data;
     }
 
     function displayResults(recipes) {
-        resultsList.innerHTML = ''; // Clear previous results
-
+        resultsList.style.display = 'block';
+    
+        resultsList.innerHTML = '';
+    
         recipes.forEach(recipe => {
             const listItem = document.createElement('li');
             listItem.textContent = recipe.name;
@@ -35,13 +42,16 @@ document.addEventListener("DOMContentLoaded", function() {
 
     async function getRecipeDetails(url) {
         const response = await fetch(`https://alltherecipeapi.onrender.com/details/${encodeURIComponent(url)}`);
+        // const response = await fetch (`http://localhost:5000/details/${encodeURIComponent(url)}`);
         const data = await response.json();
 
         return data;
     }
 
     function displayDetails(recipe) {
-        detailsContainer.innerHTML = `
+        const closeButton = '<span id="closeDetails" style="cursor: pointer; float: right; padding: 5px;">&times;</span>';
+        
+        detailsContainer.innerHTML = closeButton + `
             <h2>${recipe.name}</h2>
             <p>Rating: ${recipe.rating}</p>
             <ul>
@@ -49,5 +59,10 @@ document.addEventListener("DOMContentLoaded", function() {
             </ul>
             <a href="${recipe.url}" target="_blank">View Full Recipe</a>
         `;
+        detailsContainer.style.display = 'block';
+        
+        document.getElementById('closeDetails').addEventListener('click', function() {
+            detailsContainer.style.display = 'none'; 
+        });
     }
 });
